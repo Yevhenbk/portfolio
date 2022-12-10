687d3c9981ddda554f628489fe257520e962f717
Yevhenbk <balagutrak.jenia@gmail.com>
2022-12-10 02:03:50 +0100

    about properties update

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -283,7 +283,7 @@ const HomeTemplate: React.FC = () => {
             text-center
             flex
             flex-col
-            gap-5'
+            gap-[4rem]'
           >
             <Text 
               color='black'
@@ -307,24 +307,24 @@ const HomeTemplate: React.FC = () => {
                 Hello! My name is Yevhen and I enjoy creating things 
                 that live on the internet. My interest in web development 
                 started back in 2018 when I decided to take Python course
-                at a <a href='https://www.uc3m.es/Inicio' className='text-blue-600
-                hover:text-blue-800' target='_blank'>university</a>.
+                at a <a href='https://www.uc3m.es/Inicio' className='text-[#5A3BF8]
+                hover:text-[#5A3BC9] hover:underline' target='_blank'>university</a>.
               </p>
               <br/>
               <p>
                 After 3 years of studying Business Administration, I have decided 
                 to reconvert into a developer to become more independent by
                 following a 9 weeks coding <a href='https://4geeksacademy.com/es/inicio' 
-                className='text-blue-600 hover:text-blue-800' target='_blank'>bootcamp</a>. 
+                className='text-[#5A3BF8] hover:text-[#5A3BC9] hover:underline' target='_blank'>bootcamp</a>. 
                 I am a highly driven and self starter and enjoy talking about new projects and
                 responsibilities. Finding ways to grow and push myself is always a
                 priority for both in professional settings or in my personal endeavors.
               </p>
               <br/>
               <p>
-                Recently I worked on a <a href='https://twitter.com/worksut_' className='text-blue-600
-                hover:text-blue-800' target='_blank'>startup</a> with designers and management team to develop a
-                <a href='https://worksut.netlify.app/' className='text-blue-600 hover:text-blue-800' 
+                Recently I worked on a <a href='https://twitter.com/worksut_' className='text-[#5A3BF8]
+                hover:text-[#5A3BC9] hover:underline' target='_blank'>startup</a> with designers and management team to develop a
+                <a href='https://worksut.netlify.app/' className='text-[#5A3BF8] hover:text-[#5A3BC9] hover:underline' 
                 target='_blank'> landing page</a> for the company. Currently I am looking for new opportunities, 
                 so don't hesistate to reach out :)
               </p>
